647f051687ec30154f26c5de96cab2f2441c7c76
MndTheNerd <mndthenerd@gmail.com>
2020-01-23 03:14:41 +0300

    using const to clean up the code and make it easier to maintain vid#4

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const Color activeCardColor = Color(0x0FF1D1F33);
+const Color accentColor = Color(0x0FFEA1556);
+
+const bottomContainerHeight = 80.0;
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,18 +24,18 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0x0FF1D1F33),
+                  colour: activeCardColor,
                 )),
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0x0FF1D1F33),
+                  colour: activeCardColor,
                 ))
               ],
             ),
           ),
           Expanded(
             child: ReusableCard(
-              colour: Color(0x0FF1D1F33),
+              colour: activeCardColor,
             ),
           ),
           Expanded(
@@ -38,14 +43,20 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0x0FF1D1F33),
+                  colour: activeCardColor,
                 )),
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0x0FF1D1F33),
+                  colour: activeCardColor,
                 ))
               ],
             ),
+          ),
+          Container(
+            color: accentColor,
+            margin: EdgeInsets.only(top: 10.0),
+            height: bottomContainerHeight,
+            width: double.infinity,
           ),
         ],
       ),
@@ -60,10 +71,10 @@ class ReusableCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(15.5),
+      margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
-        borderRadius: BorderRadius.circular(12.0),
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
